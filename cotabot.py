558b4d59a8558063d741184aa0c4ea2ee02d03a6
ericklt <erick.l.t.777@gmail.com>
2019-03-07 13:18:58 -0300

    Now it's possible to create a new cota inline

---
+++ cotabot.py
@@ -0,0 +1,257 @@
+import logging
+from functools import wraps
+
+from telegram import utils
+from telegram import (ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton,
+                      ReplyKeyboardRemove, InlineKeyboardMarkup,
+                      ChatAction, ParseMode)
+from telegram.ext import (Updater, CommandHandler, CallbackQueryHandler,
+                          MessageHandler, Filters, 
+                          RegexHandler, ConversationHandler)
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+def send_typing_action(func):
+    
+    @wraps(func)
+    def command_func(bot, update, *args, **kwargs):
+        bot.send_chat_action(chat_id=update.effective_message.chat_id, action=ChatAction.TYPING)
+        return func(bot, update, *args, **kwargs)
+        
+    return command_func
+
+
+cota_chats = {}
+
+
+class Cota:
+    def __init__(self, _id, name, value=None):
+        self._id = _id
+        self.name = name
+        self.value = value
+        self.going = []
+        
+class CotaView:
+    def __init__(self, cota):
+        self.cota = cota
+        
+    def btn(self):
+        val = '' if not self.cota.value else ' - R$ {:.2f}'.format(self.cota.value)
+        btn_text = '{}'.format(self.cota.name) + val
+        return InlineKeyboardButton(btn_text, callback_data='show_cota {}'.format(self.cota._id))
+
+
+# All possible Interactive Boxes States
+
+class MainListState:
+    def __init__(self, iBox):
+        self.iBox = iBox
+        
+    def update(self, bot):
+        header = 'Lista de Cotas:'
+        if not self.iBox.cota_chat.all_cotas:
+            header = '*Não tem nenhuma cota!*'
+        cota_views = [CotaView(cota) for cota in self.iBox.cota_chat.all_cotas.values()]
+        button_list = [cota_view.btn() for cota_view in cota_views]
+        new_cota_btn = InlineKeyboardButton('Nova Cota', callback_data='new_cota')
+        close_ibox_btn = InlineKeyboardButton('Fechar', callback_data='close_ibox')
+        
+        menu = [[b] for b in button_list] + [[close_ibox_btn, new_cota_btn]]
+        
+        bot.edit_message_text(header, 
+                              reply_markup=InlineKeyboardMarkup(menu),
+                              chat_id=self.iBox.cota_chat._id, 
+                              message_id=self.iBox.message_id, 
+                              parse_mode=ParseMode.MARKDOWN)
+
+class CotaCreationState:
+    def __init__(self, iBox):
+        self.iBox = iBox
+        self.state = 0
+
+    def update(self, bot):
+        cancel_button = InlineKeyboardButton('Cancelar', callback_data='cancel_new_cota')
+        if self.state == 0:
+            header = 'Qual o nome da cota?'
+            button_list = [[cancel_button]]
+        elif self.state == 1:
+            header = 'Quanto custa a cota?'
+            button_list = [[cancel_button, InlineKeyboardButton('Pular', callback_data='skip_new_cota_value')]]
+        else:
+            return
+
+        bot.edit_message_text(header, 
+                              reply_markup=InlineKeyboardMarkup(button_list),
+                              chat_id=self.iBox.cota_chat._id, 
+                              message_id=self.iBox.message_id, 
+                              parse_mode=ParseMode.MARKDOWN)
+
+
+class InteractiveBox:
+    def __init__(self, cota_chat):
+        self.message_id = None
+        self.cota_chat = cota_chat
+        
+        self.current_state = MainListState(self)
+
+    def reset(self, bot):
+        self.current_state = MainListState(self)
+        self.update(bot)
+    
+    def update(self, bot):
+        if not self.message_id:
+            message = bot.send_message(self.cota_chat._id, "_..._", parse_mode=ParseMode.MARKDOWN)
+            self.message_id = message.message_id
+        self.current_state.update(bot)
+
+class CotaChat:
+    def __init__(self, _id):
+        self._id = _id
+        self.interactive_chat_boxes = {}
+        
+        self.next_cota_id = 0
+        self.all_cotas = {}
+        
+        self.new_cota_ibox = None
+        self.tmp_new_cota = None
+        
+    def new_ibox(self, bot):
+        iBox = InteractiveBox(self)
+        iBox.update(bot)
+        self.interactive_chat_boxes[iBox.message_id] = iBox
+
+    def remove_ibox(self, bot, message_id):
+        bot.delete_message(self._id, message_id)
+        del self.interactive_chat_boxes[message_id]
+
+    def update(self, bot):
+        for icb in self.interactive_chat_boxes.values():
+            icb.update(bot)
+
+    def start_cota_creation(self, bot, message_id):
+        iBox = self.interactive_chat_boxes[message_id]
+        iBox.current_state = CotaCreationState(iBox)
+        iBox.update(bot)
+        self.new_cota_ibox = iBox
+
+    def cota_creation_update(self, bot, message):
+        if not self.tmp_new_cota:
+            self.tmp_new_cota = Cota(self.next_cota_id, message)
+            self.new_cota_ibox.current_state.state = 1
+            self.new_cota_ibox.update(bot)
+        else:
+            try:
+                val = float(message)
+            except:
+                val = None
+            self.tmp_new_cota.value = val
+            self.submit_tmp_new_cota(bot)
+
+    def cancel_tmp_new_cota(self, bot):
+        self.tmp_new_cota = None
+        self.remove_ibox(bot, self.new_cota_ibox.message_id)
+        self.new_cota_ibox = None
+        self.new_ibox(bot)
+            
+    def submit_tmp_new_cota(self, bot):
+        self.all_cotas[self.tmp_new_cota._id] = self.tmp_new_cota
+        self.tmp_new_cota = None
+        self.next_cota_id += 1
+        self.remove_ibox(bot, self.new_cota_ibox.message_id)
+        self.new_cota_ibox = None
+        self.new_ibox(bot)
+
+def get_cota_chat(update):
+    chat_id = update.effective_chat.id
+    
+    # Add this chat if not present
+    if chat_id not in cota_chats:
+        cota_chats[chat_id] = CotaChat(chat_id)
+        
+    return cota_chats[chat_id]
+
+@send_typing_action
+def cotas(bot, update):
+    cota_chat = get_cota_chat(update)
+    cota_chat.new_ibox(bot)
+
+def handle_message(bot, update):
+    cota_chat = get_cota_chat(update)
+    if cota_chat.new_cota_ibox:
+        cota_chat.cota_creation_update(bot, update.message.text)
+    
+def new_cota(bot, update, message_id):
+    cota_chat = get_cota_chat(update)
+    cota_chat.start_cota_creation(bot, message_id)
+
+def cancel_new_cota(bot, update):
+    cota_chat = get_cota_chat(update)
+    cota_chat.cancel_tmp_new_cota(bot)
+
+def skip_cota_value(bot, update):
+    cota_chat = get_cota_chat(update)
+    cota_chat.cota_creation_update(bot, None)
+
+def close_ibox(bot, update, message_id):
+    cota_chat = get_cota_chat(update)
+    cota_chat.remove_ibox(bot, message_id)
+
+def callback_handler(bot, update):
+    query = update.callback_query
+    splt = query.data.split()
+
+    request = splt[0]
+    
+    if request == 'show_cota':
+        print('showing cota {}'.format(splt[1]))
+    elif request == 'new_cota':
+        new_cota(bot, update, query.message.message_id)
+    elif request == 'cancel_new_cota':
+        cancel_new_cota(bot, update)
+    elif request == 'skip_new_cota_value':
+        skip_cota_value(bot, update)
+    elif request == 'close_ibox':
+        close_ibox(bot, update, query.message.message_id)
+
+
+def error(bot, update, error):
+    """Log Errors caused by Updates."""
+    logger.warning('%s', error)
+
+
+
+def main():
+    # Create the Updater and pass it your bot's token.
+    # Make sure to set use_context=True to use the new context based callbacks
+    # Post version 12 this will no longer be necessary
+    updater = Updater("692336058:AAGFMBpvydprPwlYgQjwMM1QK66oH41qXfA")
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # add handlers
+    dp.add_handler(CommandHandler('cotas', cotas))
+
+    dp.add_handler(MessageHandler(Filters.text, handle_message))
+    
+    dp.add_handler(CallbackQueryHandler(callback_handler))
+
+    # log all errors
+    dp.add_error_handler(error)
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
+
